ecd2c831c13c0861fb4e05207d3013a401e562ff
Mukul Bedi <mukulbedi07@gmail.com>
2025-10-31 01:13:10 +0530

    Corrected frontend url in cors

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: "https://ourvoiceourrights.vercel.app/",
+  origin: "https://ourvoiceourrights.vercel.app",
   credentials: true,
 }));
 
